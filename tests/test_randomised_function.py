7f5fc0544d1831f5a2551e049ac2197121ed983e
= <dray3@sheffield.ac.uk>
2026-03-16 12:25:27 +0000

    added mock test for randomised_function

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from lecture import randomised_function
 
 class MyTestCase(unittest.TestCase):
@@ -6,3 +7,11 @@ class MyTestCase(unittest.TestCase):
     def test_randomised_function(self):
         self.assertTrue(randomised_function() in "software engineering")  # This will pass or fail randomly
         # TODO: Can we make this test deterministic? (HINT: Mock testing)
+
+    @patch('random.randint')
+    def test_randomised_function_mocked(self, mock_rand):
+        mock_rand.return_value = 3 # small
+        self.assertEqual('software', randomised_function())
+
+        mock_rand.return_value = 7 # not small
+        self.assertEqual('engineering', randomised_function())
